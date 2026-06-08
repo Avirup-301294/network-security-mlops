2320ebc7ada5ae881f12ce6b5d4abef50e6b1b4c
Avirup Mondal <avirupmondal1994@gmail.com>
2026-06-08 21:51:13 +0530

    Data Transformation components completed

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 
 # Config
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.components.data_validation import DataValidation
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataTransformationConfig, DataValidationConfig, TrainingPipelineConfig
 
 # Loggers & Exception
 from networksecurity.exception.exception import NetworkSecurityException
@@ -34,8 +35,23 @@ def data_validation(training_pipeline_config, data_ingestion_artifact):
         return data_validation_artifact
     except Exception as e:
            raise NetworkSecurityException(e, sys)
+    
+def data_transformation(training_pipeline_config, data_validation_artifact):
+     try:
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info(" ##### Initiate Data Transformation ##### ")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info(" ##### data Transformation completed ##### ")
+        return data_transformation_artifact
+     except Exception as e:
+           raise NetworkSecurityException(e, sys)
 
 if __name__ == "__main__":
     training_pipeline_config = TrainingPipelineConfig()
     data_ingestion_artifact = data_ingestion(training_pipeline_config)
+    print("\n\n")
     data_validation_artifact = data_validation(training_pipeline_config, data_ingestion_artifact)
+    print("\n\n")
+    data_transformation_artifact = data_transformation(training_pipeline_config, data_validation_artifact)
